da22514c58f255469e1523b61eddee3914bff2b6
jonopens <jonopenshaw@gmail.com>
2017-11-22 14:54:02 -0500

    added routes and namespacing

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:index, :show, :create]
+      resources :sites, only: [:index, :create, :show]
+      resources :pages, only: [:index, :create, :show]
+      resources :insights, only: [:index, :create, :show]
+      resources :comments, only: [:index, :create, :show]
+      resources :crawls, only: [:index, :create, :show]
+      resources :keyterms, only: [:index, :create, :show]
+    end
+  end
 end
