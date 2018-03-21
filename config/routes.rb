ce6ef8d3df9ec5a7ed1f89464e50c3afd735169a
jonopens <jonopenshaw@gmail.com>
2018-03-21 18:03:27 -0400

    remove keyterm routes

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,5 @@ Rails.application.routes.draw do
   resources :insights, only: [:index, :create, :show, :update]
   resources :comments, only: [:index, :create, :show, :update]
   resources :crawls, only: [:index, :create, :show]
-  resources :keyterms, only: [:index, :create, :show]
 
 end
